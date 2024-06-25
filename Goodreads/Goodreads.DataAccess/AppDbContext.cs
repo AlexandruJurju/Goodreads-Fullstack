c530700de21dbcbc79fdbe44d85a82a4e41bd8ec
Alex <alexandru.i.jurju@gmail.com>
2024-06-25 15:05:26 +0300

    added FluentApi persistence configuration for entities

--- Goodreads/Goodreads.DataAccess/AppDbContext.cs
+++ Goodreads/Goodreads.DataAccess/AppDbContext.cs
@@ -14,5 +14,6 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 }
---
+++ Goodreads/Goodreads.DataAccess/Configurations/BookConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Goodreads.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Goodreads.DataAccess.Configurations;
+
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Title).HasMaxLength(255);
+    }
+}
